608a669b441410d784a8d675e969c6d31c9a56e0
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-03-06 15:40:07 +0100

    Added styling to Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,30 +2,40 @@ import React, { Component } from 'react';
 import Preferences from './Preferences';
 import Matches from './Matches'
 
+import '../styles/Profile.css'
+
 class Profile extends Component {
 
 
   render() {
-    const { name } = this.props
 
 
     return (
       <div className="Profile">
 
+        <h2>Profile</h2>
         <div className="bio">
-          <h2>Profile</h2>
-          <img />
-          <p>Name: </p>
-          <p>Age: </p>
-          <p>Location: </p>
-          <p>Bio: </p>
+
+          <div className="bio-photo">
+            <img src={ this.props.image }/>
+          </div>
+
+          <div className="bio-details">
+            <p>Name: { this.props.name } </p>
+            <p>Age: { this.props.age }</p>
+            <p>Location: { this.props.location } </p>
+            <p>Bio: { this.props.bio } </p>
+          </div>
+
         </div>
+
+        <a href="" className="sniffing-btn">Start Sniffing</a>
 
         <Preferences />
 
         <Matches />
 
-        <a href="">Start Sniffing</a>
+        <a href="" className="sniffing-btn">Start Sniffing</a>
 
       </div>
     );
