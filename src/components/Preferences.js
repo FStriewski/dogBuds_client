9c1f6228dd30fef8479d3a358eb73a29f6459074
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-03-06 18:19:02 +0100

    Installed Superagent. Connected Store to Preference component

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Dogs from './Dogs'
 
+import { connect } from 'react-redux'
+
 class Preferences extends Component {
 
   render() {
@@ -8,12 +10,20 @@ class Preferences extends Component {
       <div className="Preferences">
         <h3>Preferences</h3>
 
-        <Dogs image=""/>
-        <Dogs image=""/>
-        <Dogs image=""/>
-
+        <div className="preferences-container">
+          <Dogs breed={this.props.user.preferences[0].breed } image=""/>
+          <Dogs breed={this.props.user.preferences[1].breed } image=""/>
+          <Dogs breed={this.props.user.preferences[2].breed } image=""/>
+        </div>
       </div>
     )
   }
 }
- export default Preferences;
+
+const mapStateToProps = (reduxState) => {
+  return {
+    user: reduxState.user
+  }
+}
+
+export default connect(mapStateToProps)(Preferences)
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -4,24 +4,16 @@ const initialUser = {
   location: "Amsterdam",
   bio: "Chasing tail since 1990",
   image: "http://blog.gudog.co.uk/wp-content/uploads/2015/03/gsd-and-older-man.jpg",
-  preferences: {
-    "akita": 24,
-    "basenji": 1,
-    "beagle": 24,
-    "bluetick": 1
-  }
+  preferences: [
+    { breed: "Akita", votes: 10 },
+    { breed: "Bloodhound", votes: 5 },
+    { breed: "Greyhound", votes: 3 }
+  ]
 }
 
 
 export default function(state = initialUser, action = {}) {
-  // based on the action type we will return a new state
-  switch(action.type) {
-    // some actions include a payload that we can use to update the state
-    // in this case the payload will contain a new recipe
-    case 'USER' :
-      return state
 
-    default :
-      return state
-  }
+  return state
+
 }
