0e232d518a37945c8f6ef308c891fa70a9fb4562
Scott Baxendale <scottbaxendale@hotmail.co.uk>
2018-03-07 18:23:23 +0100

    Installed Redux Thunk and imported into store.  Created FETCH USER action and handled in users reducer. Changed route of Profiles component to take user id parameter. Dispatched FETCH user action to make API call on mount.

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,86 +1,82 @@
 import React, { Component } from 'react';
 import Preferred from './Preferred'
-import { fetchDog } from '../actions/dogs'
+import { fetchUser } from '../actions/users'
 import { connect } from 'react-redux'
 
 import '../styles/Preferences.css'
 
 class Preferences extends Component {
 
-  componentWillMount() {
-    this.props.fetchDog(1)
-  }
 
 
   render() {
-
-    const {dog} = this.props.dog
+    if (!this.props.user) return null
 
     return (
       <div className="Preferences">
-
+      {console.log(this.props)}
         <h2 className="title" >Preferences</h2>
 
         <div className="preferences-container">
           <Preferred
             className="preferred"
-            breed={ this.props.user.preferences[0].breed }
-            votes={ this.props.user.preferences[0].votes }
-            image={ dog.image }
+            breed={ this.props.user.info.preferences[0].breed }
+            votes={ this.props.user.info.preferences[0].votes }
+            image=""
           />
           <Preferred
             className="preferred"
-            breed={ this.props.user.preferences[1].breed }
-            votes={ this.props.user.preferences[1].votes }
-            image={ dog.image }
+            breed={ this.props.user.info.preferences[1].breed }
+            votes={ this.props.user.info.preferences[1].votes }
+            image=""
           />
           <Preferred
             className="preferred"
-            breed={ this.props.user.preferences[2].breed }
-            votes={ this.props.user.preferences[2].votes }
-            image={ dog.image }
-          />
-        </div>
-
-        <div className="preferences-container">
-          <Preferred
-            className="preferred"
-            breed={ this.props.user.preferences[0].breed }
-            votes={ this.props.user.preferences[0].votes }
-            image={ dog.image }
-          />
-          <Preferred
-            className="preferred"
-            breed={ this.props.user.preferences[1].breed }
-            votes={ this.props.user.preferences[1].votes }
-            image={ dog.image }
-          />
-          <Preferred
-            className="preferred"
-            breed={ this.props.user.preferences[2].breed }
-            votes={ this.props.user.preferences[2].votes }
-            image={ dog.image }
+            breed={ this.props.user.info.preferences[2].breed }
+            votes={ this.props.user.info.preferences[2].votes }
+            image=""
           />
         </div>
 
         <div className="preferences-container">
           <Preferred
             className="preferred"
-            breed={ this.props.user.preferences[0].breed }
-            votes={ this.props.user.preferences[0].votes }
-            image={ dog.image }
+            breed={ this.props.user.info.preferences[0].breed }
+            votes={ this.props.user.info.preferences[0].votes }
+            image=""
           />
           <Preferred
             className="preferred"
-            breed={ this.props.user.preferences[1].breed }
-            votes={ this.props.user.preferences[1].votes }
-            image={ dog.image }
+            breed={ this.props.user.info.preferences[1].breed }
+            votes={ this.props.user.info.preferences[1].votes }
+            image=""
           />
           <Preferred
             className="preferred"
-            breed={ this.props.user.preferences[2].breed }
-            votes={ this.props.user.preferences[2].votes }
-            image={ dog.image }
+            breed={ this.props.user.info.preferences[2].breed }
+            votes={ this.props.user.info.preferences[2].votes }
+            image=""
+          />
+        </div>
+
+        <div className="preferences-container">
+          <Preferred
+            className="preferred"
+            breed={ this.props.user.info.preferences[0].breed }
+            votes={ this.props.user.info.preferences[0].votes }
+            image=""
+          />
+          <Preferred
+            className="preferred"
+            breed={ this.props.user.info.preferences[1].breed }
+            votes={ this.props.user.info.preferences[1].votes }
+            image=""
+          />
+          <Preferred
+            className="preferred"
+            breed={ this.props.user.info.preferences[2].breed }
+            votes={ this.props.user.info.preferences[2].votes }
+            image=""
           />
         </div>
 
@@ -96,10 +92,9 @@ class Preferences extends Component {
 
 const mapStateToProps = function (state, props) {
   return {
-    user: state.user,
-    dog: state
+    user: state.user
   }
 }
 
 
-export default connect(mapStateToProps, { fetchDog })(Preferences)
+export default connect(mapStateToProps, { fetchUser })(Preferences)
