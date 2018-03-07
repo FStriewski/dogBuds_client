e52d2e0c333fd4e23b9b07d3bc0a6b2b63b18866
Peter Schrammen <b.schrammen@gmail.com>
2018-03-07 10:34:27 +0100

    Route to login leeds to sign up or login form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Image from './components/Image'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-import DislikeButton from './components/Dislike'
-import LikeButton from  './components/Like'
-import ShowMatches from  './components/ShowMatches'
 import CreateUser from  './components/CreateUser'
 import Profile from './components/Profile'
 import Dogs from './components/Dogs'
@@ -14,17 +11,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <h1>DogBuds</h1>
       <Router>
        <div>
          <Route exact path="/profile" component={Profile} />
          <Route exact path="/sniffing" component={Dogs} />
+         <Route exact path="/login" component={CreateUser} />
          <Route exact path="/" render={ () => <Redirect to="/profile" /> } />
        </div>
      </Router>
-
-
-
 
       </div>
     );
